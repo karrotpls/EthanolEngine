35136fb4df0b2ce56958dd83657a5b12f4bef88b
karrotpls <karrotpls@proton.me>
2025-08-19 03:06:50 -0300

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,9 @@
+npm init -y
+npm install ethers dotenv
+
+
+RPC_URL=https://mainnet.infura.io/v3/YOUR_KEY
+PRIVATE_KEY=0xYOUR_PRIVATE_KEY
+GOVERNOR_ADDRESS=0xGovernorContractAddress
+BURN_CONTROLLER=0xBurnControllerAddress
+TOKEN_ADDRESS=0xMandalaTokenAddress
